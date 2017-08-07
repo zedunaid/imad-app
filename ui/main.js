809f9d2245fe4fdfb4aea344c2683fe99e45b7dd
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-08-08 00:06:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick=function()
     };
     
     //Make a request
-    request.open('GET','http://zunaidsorathiya9.imad.hasura-app.io',true);
+    request.open('GET','http://zunaidsorathiya9.imad.hasura-app.io/counter',true);
     request.send(null);
 };

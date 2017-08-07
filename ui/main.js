dcc1d3a3544a1c591bd67cefdcdea166fc4d64da
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-08-07 23:52:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick=function()
     
     //capture the response and store it in a variable
     request.onreadystatechange=function() {
-        if(request.readystate == XMLHttpRequest.DONE){
+        if(request.readyState == XMLHttpRequest.DONE){
             //Take some action
             if(request.status == 200)
             {

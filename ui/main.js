144215a152d9427df6197239dcc6fca551c217c8
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-08-07 16:50:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+//changing text
+
+var element=document.getElementById('main-text');
+element.innerHTML='ZHS welcomes you!';
+
+//move image
+var ele=document.getElementById('zhs');
+var marginleft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    ele.style.marginLeft=marginLeft+'px';
+}
+ele.onclick=fuction()
+{
+   var interval=setInterval(moveRight,50) ;
+}

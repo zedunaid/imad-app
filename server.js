eb6dd8f621dca2ad79f0546564caa73058d414c9
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-08-07 23:36:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,8 +118,12 @@ app.get('/:articleName', function (req, res) {
     var  articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
-
-
+//counter endpoint
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.sendFile(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

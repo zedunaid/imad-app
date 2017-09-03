12a4b70c2592a0d0ff3d9d7824978e9b9dad481b
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-09-03 17:29:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,48 @@
-//counter code
+//login check
+
+var submit=document.getElementById('submitButton');
+button.onclick=function()
+{
+    //Make a request to counter end point
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function() {
+        if(request.readyState == XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status == 200)
+            {
+                alert('Successfully Logged In');
+            }
+            
+           else if(request.status == 403)
+            {
+                alert('invalid username/password');
+            }
+            
+              else if(request.status == 500)
+            {
+                alert('OOPs ! Something Went Wrong!')l
+            }
+        }
+    };
+    
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    
+    //Make a request
+    request.open('POST','http://zunaidsorathiya9.imad.hasura-app.io/counter',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username , password: password}));
+};
+
+
+
+
+
+/*//counter code
 
 var button=document.getElementById('counter');
 button.onclick=function()
@@ -22,4 +66,4 @@ button.onclick=function()
     //Make a request
     request.open('GET','http://zunaidsorathiya9.imad.hasura-app.io/counter',true);
     request.send(null);
-};
+};*/

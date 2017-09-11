cf7bd750902f8443113fdac83581603a631849ac
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-09-12 00:25:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -270,7 +270,17 @@ app.get('/articles/:articleName', function (req, res) {
   
 });
 
-
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 
 
 app.get('/ui/style.css', function (req, res) {
